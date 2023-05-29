3310989c6487cb9ff983767ffdd276a7477250ce
hp4129889 <hp4129889@gmail.com>
2023-05-29 09:54:27 +0800

    feat: update session table action

--- src/page/Session.tsx
+++ src/page/Session.tsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
-import { Divider, Table, Typography } from "antd";
+import { Divider, Input, Table, Typography } from "antd";
 import { getSessions } from "src/api/background";
 import { ColumnsType } from "antd/es/table";
 
+const { Link } = Typography;
 interface Column {
   id: number;
   name: string;
@@ -14,67 +15,122 @@ interface Column {
   updatedAt: Date;
 }
 
-const column: ColumnsType<Column> = [
-  {
-    title: "ID",
-    key: "id",
-    dataIndex: "id",
-  },
-  {
-    title: "場次名稱",
-    key: "name",
-    dataIndex: "name",
-  },
-  {
-    title: "場次地點",
-    key: "plcae",
-    dataIndex: "place",
-  },
-  {
-    title: "人數上限",
-    key: "joinLimit",
-    dataIndex: "joinLimit",
-  },
-  {
-    title: "是否提供停車",
-    key: "isParking",
-    dataIndex: "isParking",
-    render: (value: boolean) => (value === true ? "是" : "否"),
-  },
-  {
-    title: "是否提供接駁",
-    key: "isShuttle",
-    dataIndex: "isShuttle",
-    render: (value: boolean) => (value === true ? "是" : "否"),
-  },
-  {
-    title: "創建時間",
-    key: "createdAt",
-    dataIndex: "createdAt",
-  },
-  {
-    title: "最後更新時間",
-    key: "updatedAt",
-    dataIndex: "updatedAt",
-  },
-  {
-    title: "操作",
-    render: (_, record) => {
-      return (
-        <>
-          <Typography.Link underline>編輯</Typography.Link>
-          <Divider type="vertical" />
-          <Typography.Link underline type="danger">
-            刪除
-          </Typography.Link>
-        </>
-      );
-    },
-  },
-];
-
 const Session: React.FC = () => {
-  const [sessions, setSesstions] = useState([]);
+  const [sessions, setSesstions] = useState<Column[]>([]);
+  const [editingRows, setEditingRows] = useState<number[]>([]);
+
+  const column: ColumnsType<Column> = [
+    {
+      title: "ID",
+      key: "id",
+      dataIndex: "id",
+    },
+    {
+      title: "場次名稱",
+      key: "name",
+      dataIndex: "name",
+      render: (text, record) => {
+        return <></>;
+      },
+    },
+    {
+      title: "場次地點",
+      key: "plcae",
+      dataIndex: "place",
+    },
+    {
+      title: "人數上限",
+      key: "joinLimit",
+      dataIndex: "joinLimit",
+    },
+    {
+      title: "是否提供停車",
+      key: "isParking",
+      dataIndex: "isParking",
+      render: (value: boolean) => (value === true ? "是" : "否"),
+    },
+    {
+      title: "是否提供接駁",
+      key: "isShuttle",
+      dataIndex: "isShuttle",
+      render: (value: boolean) => (value === true ? "是" : "否"),
+    },
+    {
+      title: "創建時間",
+      key: "createdAt",
+      dataIndex: "createdAt",
+    },
+    {
+      title: "最後更新時間",
+      key: "updatedAt",
+      dataIndex: "updatedAt",
+    },
+    {
+      title: "操作",
+      render: (_, record) => {
+        return (
+          <>
+            {editingRows.find((id) => record.id === id) ? (
+              <>
+                <Link
+                  onClick={() =>
+                    setEditingRows((pre) =>
+                      pre.filter((rowId) => rowId !== record.id)
+                    )
+                  }
+                >
+                  儲存
+                </Link>
+                <Divider type="vertical" />
+                <Link underline type="danger">
+                  取消
+                </Link>
+              </>
+            ) : (
+              <>
+                <>
+                  <Link
+                    underline
+                    onClick={() => setEditingRows((pre) => [...pre, record.id])}
+                  >
+                    編輯
+                  </Link>
+                  <Divider type="vertical" />
+                  <Link underline type="danger">
+                    刪除
+                  </Link>
+                </>
+              </>
+            )}
+          </>
+        );
+      },
+    },
+  ];
+
+  const onAddRow = () => {
+    // const lastSession = sessions[sessions.length - 1];
+    // const { id } = lastSession;
+    // const newRow: Column = {
+    //   id: id + 1,
+    //   name: "",
+    //   place: "",
+    //   joinLimit: 0,
+    //   isParking: false,
+    //   isShuttle: false,
+    //   createdAt: new Date(),
+    //   updatedAt: new Date(),
+    // };
+    // setSesstions([...sessions, newRow]);
+  };
+
+  const tableFooter = () => {
+    return (
+      <Link underline onClick={onAddRow}>
+        新增欄位
+      </Link>
+    );
+  };
 
   useEffect(() => {
     getSessions()
@@ -87,8 +143,15 @@ const Session: React.FC = () => {
         console.error(err);
       });
   }, []);
+
   return (
-    <Table size="small" rowKey="id" columns={column} dataSource={sessions} />
+    <Table
+      size="small"
+      rowKey="id"
+      columns={column}
+      dataSource={sessions}
+      footer={tableFooter}
+    />
   );
 };
 
